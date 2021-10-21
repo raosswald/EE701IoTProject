e40a5ef4c0290f87eab258a4afdc4e394db1b898
Ryan O MacBook <ryanossw@buffalo.edu>
2021-10-21 15:12:19 -0400

    Started Git Tree

---
+++ GUIApp.py
@@ -0,0 +1,61 @@
+import os
+import random
+imb port urllib
+import sys
+import folium
+from folium import features
+from PyQt5.QtWidgets import QMainWindow, QApplication, QVBoxLayout, QWidget, QPushButton
+from PyQt5.QtWebEngineWidgets import QWebEngineView
+
+#https://zetcode.com/pyqt/qwebengineview/
+class Example(QWidget):
+
+    def __init__(self):
+        super().__init__()
+
+        self.initUI()
+
+    def initUI(self):
+
+        vbox = QVBoxLayout(self)
+
+        self.webEngineView = QWebEngineView()
+        self.loadPage()
+
+        NewMap = QPushButton("Update Map")
+        NewMap.clicked.connect(self.GenerateNewMap)
+
+        button = QPushButton("Reload Map")
+        button.clicked.connect(self.loadPage)
+
+        vbox.addWidget(self.webEngineView)
+        vbox.addWidget(button)
+        vbox.addWidget(NewMap)
+
+        self.setLayout(vbox)
+
+        self.setGeometry(300, 300, 550, 450)
+        self.setWindowTitle('MapMyAir')
+        self.show()
+
+    def loadPage(self):
+
+        with open('index.html', 'r') as f:
+
+            html = f.read()
+            self.webEngineView.setHtml(html)
+
+    def GenerateNewMap(self):
+        
+        map = folium.Map(location=[43, -78.7849])
+        map.save("index.html")
+
+
+
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    window = Example()
+    window.show()
+    sys.exit(app.exec_())
---
+++ TestMap.py
@@ -0,0 +1,6 @@
+import os
+import folium
+
+
+map = folium.Map(location=[53.0043, -78.7849])
+map.save("index.html")
